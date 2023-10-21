5d7030f5674b10e2d87a062ec01f9d83a4864c0c
alexandremarcopoulos <alexandremarcopoulos28@gmail.com>
2023-10-21 02:23:49 -0300

    Inclusao das funcoes de transferencia de valores entre contas

--- src/main/java/com/desafio/apitransferencia/service/TransacaoService.java
+++ src/main/java/com/desafio/apitransferencia/service/TransacaoService.java
@@ -1,10 +1,13 @@
 package com.desafio.apitransferencia.service;
 
+import com.desafio.apitransferencia.domain.transacao.Transacao;
 import com.desafio.apitransferencia.domain.usuario.Cliente;
 import com.desafio.apitransferencia.dto.TransacaoDTO;
 import com.desafio.apitransferencia.repository.TransacaoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 public class TransacaoService {
@@ -16,6 +19,22 @@ public class TransacaoService {
     private TransacaoRepository transacaoRepository;
 
     public void criaTransacao(TransacaoDTO transacao) throws Exception{
-        Cliente cliente = this.clienteService.findClienteById(transacao.numeroContaPagador());
+        Cliente clientePagador = this.clienteService.findClienteById(transacao.numeroContaPagador());
+        Cliente clienteRecebedor = this.clienteService.findClienteById(transacao.numeroContaRecebedor());
+
+        clienteService.validaTransacao(clientePagador, transacao.valorTransacao());
+
+        Transacao transacaoCliente = new Transacao();
+        transacaoCliente.setValorTransacao(transacao.valorTransacao());
+        transacaoCliente.setClientePagador(clientePagador);
+        transacaoCliente.setClienteRecebedor(clienteRecebedor);
+        transacaoCliente.setDataHoraTransacao(LocalDateTime.now());
+
+        clientePagador.setSaldoConta(clientePagador.getSaldoConta().subtract(transacao.valorTransacao()));
+        clienteRecebedor.setSaldoConta(clienteRecebedor.getSaldoConta().add(transacao.valorTransacao()));
+
+        this.transacaoRepository.save(transacaoCliente);
+        this.clienteService.salvaCliente(clientePagador);
+        this.clienteService.salvaCliente(clienteRecebedor);
     }
 }
