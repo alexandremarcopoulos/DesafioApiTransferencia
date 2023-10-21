a4e9de43c6a767824ae9076ed5d90e52478cc58a
alexandremarcopoulos <alexandremarcopoulos28@gmail.com>
2023-10-21 01:12:40 -0300

    Criacao de classe de servico para implementacao de transferencia e validacao de transferencia com saldo suficiente ou menor que 1000 reais

---
+++ src/main/java/com/desafio/apitransferencia/service/ClienteService.java
@@ -0,0 +1,34 @@
+package com.desafio.apitransferencia.service;
+
+import com.desafio.apitransferencia.domain.usuario.Cliente;
+import com.desafio.apitransferencia.repository.ClienteRepository;
+import org.apache.catalina.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
+public class ClienteService {
+
+    @Autowired
+    private ClienteRepository clienteRepositorio;
+
+    public void validaTransacao(Cliente clienteTranferidor, BigDecimal valor) throws Exception {
+        //Verifica se o cliente solicitante tem o saldo suficiente para realizar a transação
+
+        if (clienteTranferidor.getSaldoConta().compareTo(valor) < 0) {
+            throw new Exception("Cliente não tem saldo para realizar a transação");
+        } else if (clienteTranferidor.getSaldoConta().compareTo(valor) > 1000) {
+            throw new Exception("Transação com valor superior a R$ 1000.00 Reais");
+        }
+
+    }
+    public Cliente findClienteById (int numeroConta) throws Exception {
+        return this.clienteRepositorio.findClienteByNumeroConta(numeroConta).orElseThrow(() -> new Exception("Usuário não encontrado"));
+    }
+
+    public void salvaCliente(Cliente cliente){
+        this.clienteRepositorio.save(cliente);
+    }
+}
