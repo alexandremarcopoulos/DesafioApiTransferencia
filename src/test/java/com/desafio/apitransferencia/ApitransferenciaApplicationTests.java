e47e780e106e6d13340931807f1245bd0f970171
alexandremarcopoulos <alexandremarcopoulos28@gmail.com>
2023-10-20 23:40:51 -0300

    Commit inicial/config

---
+++ src/test/java/com/desafio/apitransferencia/ApitransferenciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.apitransferencia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApitransferenciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
