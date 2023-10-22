73efdc42bb8f0824c4f5790b8477ff6be746aa64
alexandremarcopoulos <alexandremarcopoulos28@gmail.com>
2023-10-22 20:56:53 -0300

    Retirada dos imports desnecessarios

--- src/test/java/com/desafio/apitransferencia/controller/ClienteControllerTest.java
+++ src/test/java/com/desafio/apitransferencia/controller/ClienteControllerTest.java
@@ -6,23 +6,18 @@ import com.desafio.apitransferencia.service.ClienteService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.mock.web.MockMultipartFile;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class ClienteControllerTest {
